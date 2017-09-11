5fab243028b164857201caa1a7f832ccbaded684
SE UNRC <fbrusatti@dc.exa.unrc.edu.ar>
2017-09-11 18:41:37 -0300

    Install and configure Spark

--- src/main/java/twoUsersGame/App.java
+++ src/main/java/twoUsersGame/App.java
@@ -1,13 +1,14 @@
 package twoUsersGame;
+import static spark.Spark.*;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      get("/hello", (req, res) -> "Hello World!");
     }
 }
