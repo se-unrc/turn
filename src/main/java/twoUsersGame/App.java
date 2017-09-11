441eaa2ed5689c6dcc037807941615d1d012b319
SE UNRC <fbrusatti@dc.exa.unrc.edu.ar>
2017-09-11 18:47:07 -0300

    Install Mustache as view engine

--- src/main/java/twoUsersGame/App.java
+++ src/main/java/twoUsersGame/App.java
@@ -1,14 +1,28 @@
 package twoUsersGame;
+
 import static spark.Spark.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
 /**
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-      get("/hello", (req, res) -> "Hello World!");
-    }
+public class App {
+  public static void main(String[] args) {
+    Map map = new HashMap();
+    map.put("name", "Sam");
+    map.put("value", 1000);
+    map.put("taxed_value", 1000 - (1000 * 0.4));
+    map.put("in_ca", true);
+
+    get("/hello", (req, res) -> {
+        return new ModelAndView(map, "./views/hello.mustache");
+      }, new MustacheTemplateEngine()
+    );
+  }
 }
